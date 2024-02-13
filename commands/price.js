4b9d1b7ad3ea0035cd0b8ab9d57295a1a4e6da3c
hapticfish <jquinmail@gmail.com>
2024-02-13 20:28:57 -0300

    added code for price, not complete

---
+++ commands/price.js
@@ -0,0 +1,9 @@
+module.exports = {
+    name: 'price',
+    exexute(message, args) {
+        console.log(`Message received: ${message.content}`);
+        console.log('!price command received'); // Confirm command is detected
+
+        
+    }
+}
