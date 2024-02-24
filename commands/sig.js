e066da78cac986ea22ab48c799df30e1bc04ef11
hapticfish <jquinmail@gmail.com>
2024-02-23 23:51:10 -0300

    started code for sig or signal command

---
+++ commands/sig.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    name: 'sig',
+    execute(message, args) {
+        console.log(`Message received: ${message.content}`);
+        console.log('!sig command received'); // Confirm command is detected
+
+        
+    }
+}
