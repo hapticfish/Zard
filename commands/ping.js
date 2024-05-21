7eb6a748d8f1b5ec4d6d4d6f192d31689f8fecd7
hapticfish <jquinmail@gmail.com>
2024-05-21 17:07:27 -0300

    added command loggign for ping/pong

--- commands/ping.js
+++ commands/ping.js
@@ -1,16 +1,59 @@
 // commands/ping.js
 const { SlashCommandBuilder } = require('@discordjs/builders');
+const commandUsageModel = require("../database/commandUsageModel");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('ping')
         .setDescription('Replies with Pong!'),
     async execute(interaction) {
-        // Log the command and user details
-        console.log(`Slash command received: /${interaction.commandName} by ${interaction.user.tag}`);
 
-        // Reply to the interaction
-        await interaction.reply('Pong!');
-        console.log('Replied with Pong!');
+        const startTime = Date.now(); // Start time for response time calculation
+
+        try{
+            console.log(`Slash command received: /${interaction.commandName} by ${interaction.user.tag}`);
+            await interaction.reply('Pong!');
+            console.log('Replied with Pong!');
+
+            const endTime = Date.now();
+            const responseTime = endTime - startTime;
+
+            // Log successful command usage
+            await commandUsageModel.logCommandUsage({
+                userID: interaction.user.id,
+                commandID: interaction.commandId,
+                commandName: interaction.commandName,
+                description: 'Replies with Pong!',
+                timestamp: new Date(),
+                guildID: interaction.guildId,
+                channelID: interaction.channelId,
+                parameters: JSON.stringify(interaction.options.data),
+                success: true,
+                errorCode: null,
+                responseTime: responseTime
+            });
+
+        }catch (error){
+            console.error('Error running Ping command:', error);
+            await interaction.reply('Ping failed. Somethings wrong here...');
+
+            const endTime = Date.now();
+            const responseTime = endTime - startTime;
+
+            // Log failed command usage
+            await commandUsageModel.logCommandUsage({
+                userID: interaction.user.id,
+                commandID: interaction.commandId,
+                commandName: interaction.commandName,
+                description: 'Replies with Pong!',
+                timestamp: new Date(),
+                guildID: interaction.guildId,
+                channelID: interaction.channelId,
+                parameters: JSON.stringify(interaction.options.data),
+                success: false,
+                errorCode: error.message,
+                responseTime: responseTime
+            });
+        }
     }
 };
