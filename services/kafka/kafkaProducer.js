87c5dbf015460ce15bb75bde1fe46461d8fdf7e0
hapticfish <jquinmail@gmail.com>
2024-06-03 00:50:17 -0300

    commit remaining

--- services/kafka/kafkaProducer.js
+++ services/kafka/kafkaProducer.js
@@ -1,16 +1,23 @@
 // kafkaProducer.js
 const { Kafka } = require('kafkajs');
+require('dotenv').config({ path: '../.env' });
 
 const kafka = new Kafka({
     clientId: 'Zard_v001',
-    brokers: ['kafka1:9092', 'kafka2:9092']
+    brokers: ['localhost:9092', 'localhost:9093'],
+    ssl: true,
+    sasl: {
+        mechanism: 'scram-sha-256',
+        username: process.env.KAFKA_BROKER_USERNAME,
+        password: process.env.KAFKA_BROKER_PASSWORD
+    }
 });
 
 const producer = kafka.producer();
 
 const connectProducer = async () => {
     await producer.connect();
-    console.log("Kafka Producer connected");
+    console.log("Kafka Producer connected with SASL_SS");
 };
 
 const send = async ({ topic, messages }) => {
@@ -26,3 +33,4 @@ module.exports = { connectProducer, send, producer };
 //TODO get Brokers and signed up for kafka the clusters to which they connect
 //todo configure kafka client
 
+//todo GO BACK TO CONFIGURING KAFKA BROKER!!!!
